dd9dabdd8789f846da68328b381c4fd0d6178417
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-02-03 16:28:42 +0200

    Add task

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-'use strict'
+"use strict";
 
-function logMessage() {
-  console.log("messege: click")
-}
+// function logMessage() {
+//   console.log("messege: click")
+// }
 
-const [button]=document.getElementsByTagName("button");
+// const [button]=document.getElementsByTagName("button");
 
-button.addEventListener("click",logMessage);
+// button.addEventListener("click",logMessage);
 
-const testSelector = document.querySelector('#test')
+// const testSelector = document.querySelector('#test')
 
-const test = document.getElementById("test");
-console.log(test);
+// const test = document.getElementById("test");
+// console.log(test);
 
-const [input] = document.getElementsByName("input");
-console.log(input);
+// const [input] = document.getElementsByName("input");
+// console.log(input);
 
-const lis = document.getElementsByClassName("item")
-console.log(lis);
+// const lis = document.getElementsByClassName("item")
+// console.log(lis);
 
-const h1 = document.querySelector('h1')
-console.log(h1)
+// const h1 = document.querySelector('h1')
+// console.log(h1)
 
+// const ps = document.querySelector("p");
+// console.log(ps)
 
-const ps = document.querySelector("p");
-console.log(ps)
+const [img1] = document.images;
+const [img2] = document.getElementsByTagName("img");
+const img3 = document.querySelector("img");
+
+const ps = document.querySelectorAll(".article:nth-child(3)>p");
+
+const h2 = document.querySelectorAll(".article:last-child h2");
