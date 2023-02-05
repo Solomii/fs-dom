a4954c8497dd010cc2044c2735892ce491bb3d61
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-02-05 18:40:36 +0200

    Add style to slider-container

--- index.js
+++ index.js
@@ -25,10 +25,27 @@
 // const ps = document.querySelector("p");
 // console.log(ps)
 
-const [img1] = document.images;
-const [img2] = document.getElementsByTagName("img");
-const img3 = document.querySelector("img");
+//!!!!! task
 
-const ps = document.querySelectorAll(".article:nth-child(3)>p");
+// const [img1] = document.images;
+// const [img2] = document.getElementsByTagName("img");
+// const img3 = document.querySelector("img");
 
-const h2 = document.querySelectorAll(".article:last-child h2");
+// const ps = document.querySelectorAll(".article:nth-child(3)>p");
+
+// const h2 = document.querySelectorAll(".article:last-child h2");
+
+//task 2
+
+const db = [
+  "https://ggsc.s3.amazonaws.com/images/made/images/uploads/The_Science-Backed_Benefits_of_Being_a_Dog_Owner_600_400_int_c1-2x.jpg",
+  "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/golden-retriever-royalty-free-image-506756303-1560962726.jpg?crop=1.00xw:0.756xh;0,0.0756xh&resize=768:*",
+  "https://www.dogstrust.org.uk/images/1440x1080/assets/2022-07/188854sponsordogeric.jpg"
+];
+
+const image = document.querySelector(".slide>img");
+image.src = db[0]
+
+const [prevBtn, nextBtn] = document.querySelectorAll(
+  ".slider-container button"
+);
